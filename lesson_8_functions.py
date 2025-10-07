ba8c9a757b68c57774d06277487c9c83b583ab5b
zefirkasokoladnaa8 <161081379+zefirkasokoladnaa8@users.noreply.github.com>
2025-10-07 18:22:46 +0300

    Create lesson_8_functions.py

---
+++ lesson_8_functions.py
@@ -0,0 +1,10 @@
+# Task 1 A simple function.
+# Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
+# The function should then print "My favorite movie is named {name}".  
+
+def favorite_movie(name):
+    print(f"My favorite movie is named {name}")
+favorite_movie("Pirates of the Caribbean")
+
+# ----------------------------------------------------------------------------------------------------------------
+
